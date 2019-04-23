a4cfcbba36311c191589eda2127201d9aabefa2c
MauroRodriguez94 <mauro.rodriguez@mercadolibre.com>
2019-04-23 10:53:01 -0300

    Update DummyFullScreenModal.java

--- ui/src/test/java/com/mercadolibre/android/ui/widgets/DummyFullScreenModal.java
+++ ui/src/test/java/com/mercadolibre/android/ui/widgets/DummyFullScreenModal.java
@@ -1,5 +1,6 @@
 package com.mercadolibre.android.ui.widgets;
 
+import android.content.Context;
 import android.view.View;
 
 import com.mercadolibre.android.ui.R;
@@ -18,6 +19,15 @@ public class DummyFullScreenModal extends FullScreenModal {
     @Override
     public int getContentView() {
         return R.layout.ui_layout_melidialog_test;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        try {
+            super.onAttach(context);
+        } catch (Exception e) {
+            //do nothing
+        }
     }
 
     @Override
